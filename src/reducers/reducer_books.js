dd871b261f71e6b0faebc5292834144f405342bd
Rachel <rpavilanis@avvo.com>
2017-04-04 08:49:32 -0700

    added BooksReducer

---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function() {
+  return [
+    { title: 'Mistborn' },
+    { title: 'Harry Potter and the Half-Blood Prince' },
+    { title: 'Uprooted' },
+    { title: 'Seraphine' }
+  ]
+}
