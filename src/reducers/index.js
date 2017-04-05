e8b123d759998ae041412e435a5512ef67151ca1
Rachel <rpavilanis@avvo.com>
2017-04-05 15:45:47 -0700

    finished adding charts and styling them

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 import WeatherReducer from './reducer_weather';
 
 const rootReducer = combineReducers({
-  state: (state = {}) => state
+  weather: WeatherReducer
 });
 
 export default rootReducer;
